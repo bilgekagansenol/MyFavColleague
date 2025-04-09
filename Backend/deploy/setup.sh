d5eadf0f84665458bd39b81d1be48c639d3ee933
bilgekagansenol <bilge.kagan.senoll@gmail.com>
2025-04-09 15:46:47 +0300

    deploy configuration

--- Backend/deploy/setup.sh
+++ Backend/deploy/setup.sh
@@ -2,41 +2,49 @@
 
 set -e
 
-# TODO: Set to URL of git repo.
+# GIT repo URL (senin projen)
 PROJECT_GIT_URL='https://github.com/bilgekagansenol/MyFavColleague.git'
 
+# Kurulumun yapılacağı ana dizin
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-# Set Ubuntu Language
+# Ubuntu dil ayarı (gerekli değilse kaldırabilirsin)
 locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
-echo "Installing dependencies..."  
+# Gerekli paketlerin kurulumu
+echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
+# Uygulama klasörünü oluştur
 mkdir -p $PROJECT_BASE_PATH
 
+# Repo'yu doğru branch ile klonla
 git clone --branch bilgebackend $PROJECT_GIT_URL $PROJECT_BASE_PATH
+
+# Backend klasörüne gir (orası senin esas kodların)
 cd $PROJECT_BASE_PATH/Backend
 
-python3 -m venv $PROJECT_BASE_PATH/env
+# Sanal ortam oluştur
+python3 -m venv env
 
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Gerekli Python paketlerini yükle
+env/bin/pip install --upgrade pip
+env/bin/pip install -r requirements.txt uwsgi==2.0.21
 
-# Run migrations
-$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
+# Veritabanı işlemleri
+env/bin/python manage.py migrate
 
-# Setup Supervisor to run our uwsgi process.
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+# Supervisor servisini ayarla
+cp deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
-# Setup nginx to make our application accessible.
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-rm /etc/nginx/sites-enabled/default
+# Nginx yapılandırması
+cp deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+rm -f /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
 
-echo "DONE! :)"
+echo "✅ Deployment tamamlandı!"
