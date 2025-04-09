c334c59250380ac35e33e47b32717bad146a0bf9
bilgekagansenol <bilge.kagan.senoll@gmail.com>
2025-04-09 16:23:09 +0300

    update update.sh

--- Backend/deploy/update.sh
+++ Backend/deploy/update.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api/Backend'
 
 cd $PROJECT_BASE_PATH
-git pull
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+git pull origin bilgebackend
+
+./env/bin/python manage.py migrate
+./env/bin/python manage.py collectstatic --noinput
+
 supervisorctl restart profiles_api
 
-echo "DONE! :)"
+echo "✅ GUNCELLEME TAMAMLANDI! "
